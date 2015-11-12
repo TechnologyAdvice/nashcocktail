21b3d9ee0803a26d9c4a9a4e4ef05a2dee819ef9
Kyle Turco <kyleturco@gmail.com>
2015-11-12 16:18:44 -0600

    add links to header nav

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,16 @@
-// main.js
+$(document).ready(initialize);
+
+function initialize() {
+  $('a.main-nav').on('click', clickShowNavMenu);
+}
+
+function clickShowNavMenu(e) {
+  e.preventDefault();
+  if($(window).width() < 768) {
+    if($('ul.navbar-right').is(':hidden')) {
+      $('ul.navbar-right').slideDown();
+    } else {
+      $('ul.navbar-right').slideUp();
+    }
+  }
+}
