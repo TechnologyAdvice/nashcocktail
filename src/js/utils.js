7a6ada9a4cadc00174e6914d0108eca666f18962
athurman <athurman215@gmail.com>
2015-11-23 10:36:38 -0600

    add contentful api client and jquery methods to update content

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,1 +1,29 @@
-// utils.js
+contentfulClient.spaces.getEntries('mgvhfvj4jv6c')
+  .done(function( res ) {
+    console.log('done');
+    var eventEntry;
+    if (res.items.length > 0) {
+      console.log(res.items);
+      eventEntry = res.items[0];
+      updateRegistrationLinks(eventEntry.fields.eventLink);
+      updateAboutFeaturedCompany(eventEntry.fields.aboutEventSponsor);
+      updateEventDate(eventEntry.fields.eventStartTime);
+    } else {
+      updateRegistrationLinks('http://www.eventbrite.com/o/technologyadvice-8632648480');
+    }
+  });
+
+function updateRegistrationLinks(url) {
+  var $registryLinks = $('.event-register-link');
+  for (var i = 0; i <= $registryLinks.length; i++) {
+    $($registryLinks[i]).attr('href', url);
+  }
+}
+
+function updateAboutFeaturedCompany(content) {
+  $('#about-featured-company').empty().append(content);
+}
+
+function updateEventDate(startDate) {
+
+}
