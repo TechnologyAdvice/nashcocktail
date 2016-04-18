ef78effbe0e52c78e9cebfefb773cd5ab199cb28
Kyle Turco <kyleturco@gmail.com>
2016-04-18 11:24:13 -0500

    add main.js file and header src

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(initialize);
+
+function initialize() {
+  $('.header-link').on('click', smoothScroll);
+}
+
+function smoothScroll(e) {
+  e.preventDefault();
+  $('html, body').animate({
+      scrollTop: $(this.hash).offset().top
+  }, 700);
+  return false;
+}
