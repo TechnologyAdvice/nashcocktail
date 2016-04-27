18e56dcea215b236460b28a28dcd5590feca90b9
Josh Habdas <jhabdas@gmail.com>
2016-04-27 13:37:50 -0500

    fix(forms): provide more robust validation with mobile support

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,12 @@
-$(document).ready(initialize)
+$(function() {
 
-function initialize() {
 
+  // TODO: Move sections into separate files for better separation of concerns and pull in by page conditionally
+
+
+  //
+  // Smooth scroll
+  // --------------------------------------------------
   function smoothScroll(el, to, duration) {
       if (duration < 0) {
           return;
@@ -15,9 +20,45 @@ function initialize() {
           }
       }.bind(this), 10);
   }
+
+  // Activate smooth scroll for Sponsor Sign Up link click event
   $('.sponsor-register-link').on('click', function(e) {
       e.preventDefault();
       smoothScroll($(window), $($(e.currentTarget).attr('href')).offset().top, 200);
   });
 
-}
+  //
+  // Register as a Sponsor ActOn form validation
+  // --------------------------------------------------
+  $('#sponsorForm').isHappy({
+    fields: {
+      // keys are selectors, i.e. '[name=name]'
+      '#sponsorContact': {
+        required: true,
+        message: 'Please provide a primary contact name.'
+      },
+      '#sponsorEmail': {
+        required: true,
+        message: 'Please provide a valid contact email.',
+        test: function(val) {
+          return /.+@.+\..+/.test(val);
+        }
+      },
+      '#sponsorPhone': {
+        required: true,
+        message: 'Please enter a valid phone number (ex: 555-555-5555)',
+        test: function(val) {
+          return /\d{3}[\-]\d{3}[\-]\d{4}/.test(val);
+        }
+      },
+      '#sponsorCompany': {
+        required: true,
+        message: 'Please provide company or affiliate name.'
+      }
+    }
+  })
+})
+
+
+
+
--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,5 @@
 # Do not delete this line. This main JS file needs this front matter
 ---
 {% include_relative vendor/zepto/zepto.min.js %}
+{% include_relative vendor/happyjs/happy.js %}
 {% include_relative app.js %}
